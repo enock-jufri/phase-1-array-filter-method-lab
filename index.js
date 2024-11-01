54b4d7bcc31e205f4b1d186dfe5ae18a18422601
enock-jufri <enockjufri@gmail.com>
2024-11-01 11:25:32 +0300

    filter function

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(collection, match){
+     
+    return collection.filter(name => {
+        
+        if(name === match){
+            return name
+        }
+        else if(name.toLowerCase()===match.toLowerCase()){
+            return name
+        }else{
+            return null
+        }
+        }
+    )
+}
+function fuzzyMatch(collection, match){
+    return collection.filter(name =>{
+       let newName = name.slice(0,2)
+        if(newName===match){
+            return name
+        }
+    })
+}
+function matchName(collection, match){
+    return collection.filter(collection => collection.name===match)
+}
